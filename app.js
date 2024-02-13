d2f68325bfb62707d44a70324b9e1293b844b527
Tatiane Regina <tatiane.santos@gmail.com>
2024-02-13 18:12:57 -0300

    Criar função alteraTexto

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
-let titulo = document.querySelector('h1');
-titulo.innerHTML= 'Hora do Desafio';
-let paragrafo = document.querySelector('p');
-paragrafo.innerHTML = 'Infome um número de 0 a 100';
+//let titulo = document.querySelector('h1');
+//titulo.innerHTML= 'Hora do Desafio';
+//let paragrafo = document.querySelector('p');
+//paragrafo.innerHTML = 'Infome um número de 0 a 100';
+
+alteraTexto("h1", 'Hora do Desafio');
+alteraTexto("p", "Infome um número de 0 a 100");
+
 function console1(){
     console.log('O botão foi clicado');
 };
@@ -14,3 +18,8 @@ function cidade(){
     cidadeBrasil = prompt('Informe uma cidade do Brasil:');
     alert(`Estive em ${cidadeBrasil} e lembrei de você`);
 }
+
+function alteraTexto(tag, texto){
+    let titulo = document.querySelector(tag);
+    titulo.innerHTML= texto;    
+};
